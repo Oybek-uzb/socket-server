9711b8863d1dfebc6598e0eebe414817630adb34
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-07-21 16:34:05 +0500

    server initial setups

---
+++ src/server.gateway.ts
@@ -0,0 +1,13 @@
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ServerGateway {
+  @WebSocketServer()
+  server;
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, data: string): void {
+    this.server.emit('message', data);
+  }
+}
