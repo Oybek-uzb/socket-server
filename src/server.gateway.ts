2999efcaf61c0d94a6ec96f958b6a3fa971df49e
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-07-21 20:05:16 +0500

    resending logic done

--- src/server.gateway.ts
+++ src/server.gateway.ts
@@ -10,10 +10,55 @@ export class ServerGateway {
   @WebSocketServer()
   server;
 
+  private sockets = new Map();
+
   @SubscribeMessage('connected')
   handleConnect(client: Socket, data: string): void {
-    console.log(client, data);
+    this.sockets.set(client.id, {
+      isResponded: false,
+      attempts: 0,
+      repeater: null,
+    });
 
-    this.server.emit('connected', 'from server');
+    this.checkAndSend(client.id);
+
+    this.sockets.get(client.id).repeater = setInterval(() => {
+      console.log(this.sockets);
+      if (
+        !this.sockets.get(client.id).isResponded &&
+        this.sockets.get(client.id).attempts != 3
+      ) {
+        this.checkAndSend(client.id);
+      } else {
+        if (this.sockets.get(client.id).isResponded) {
+          console.log(`client with id: ${client.id} has responded!`);
+        } else {
+          console.log(`client with id: ${client.id} has not responded!`);
+        }
+        clearInterval(this.sockets.get(client.id).repeater);
+      }
+    }, 10000);
+  }
+
+  @SubscribeMessage('random')
+  handleRandomMessage(client: Socket, data: string): void {
+    console.log(client.id, data);
+
+    this.sockets.set(client.id, {
+      ...this.sockets.get(client.id),
+      isResponded: true,
+      attempts: 3,
+    });
+    console.log(`random ${this.sockets}`);
+  }
+
+  checkAndSend(socketId) {
+    console.log(this.sockets);
+    this.server.emit('conn-checked', 'from server');
+    this.sockets.set(socketId, {
+      ...this.sockets.get(socketId),
+      isResponded: false,
+      attempts: this.sockets.get(socketId).attempts + 1,
+    });
   }
 }
